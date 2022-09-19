20aaeb714041c606fcdf26776bd0a35a02af1cc7
CalebKornfein <caleb.kornfein@gmail.com>
2022-09-18 22:58:06 -0700

    calculate spearman for ATAC data

--- spearman.py
+++ spearman.py
@@ -2,6 +2,9 @@ from scipy import stats
 from helpers import load_labels, generate_dataset_given_feature_names
 import pandas as pd
 import csv
+import scipy.sparse as sp
+import numpy as np
+from tqdm import tqdm
 
 def calculate_spearman():
     data = []
@@ -23,26 +26,41 @@ def calculate_spearman():
 
                 data.append(data_row)
     
-    df = pd.DataFrame(data, columns=columns)
+    # load atac data and feature names
+    atac = sp.load_npz('data/atac.mat/atac.mat.npz')
+
+    f = open('data/atac.mat/atac.mat.rownames.txt', 'r')
+    atac_features = [line.strip() for line in f.readlines()]
+
+    for row in tqdm(range(atac.shape[0])):
+        vals = np.array(atac[row, ].todense()).flatten()
+        feature = atac_features[row]
+
+        spearman = stats.spearmanr(hiv, vals)
+
+        data_row = [feature, 'atac', spearman.correlation, abs(spearman.correlation), spearman.pvalue]
+        data.append(data_row)
+
+    df = pd.DataFrame(data, columns=columns).sort_values(by='abs_spearman', ascending=False)
     df.to_csv('spearman.csv', index=False)
+    print("Wrote spearman.csv")
 
 def select_top_features(n=200, type=None):
-    df = pd.read_csv('spearman.csv').sort_values(by='abs_spearman', ascending=False)
-
+    df = pd.read_csv('spearman.csv')
     if type == None:
         top_features = df.head(n)
     else:
-        top_features = df[df['dataset'] == type].head(n)
+        top_features = df[df['dataset'].isin(type)].head(n)
     
     top_features = set(top_features.feature.values)
     return top_features
 
 if __name__ == "__main__":
     # 1) Calculate spearman coeffecients for all features against HIV
-    #calculate_spearman()
+    calculate_spearman()
 
     # 2) Choose the top 200 features to model with the highest absolute spearman correlation to model with
-    feature_names = select_top_features(200)
+    feature_names = select_top_features(1000)
 
     # 3) Generate a dataset of exclusively these top 200 features
     generate_dataset_given_feature_names(feature_names, 'multiomics.csv')
